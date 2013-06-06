5ed29140335abb8a8107403101e677b7add9962e
toby82 <44012573@qq.com>
2013-06-06 18:13:45 +0800

    11111

--- autoinstall.sh
+++ autoinstall.sh
@@ -12,3 +12,12 @@ if [ $? = 0 ];then
 echo "set success"	
 apt-get -y update && apt-get -y install nova-compute nova-network
 fi
+if [ $? = 0 ];then
+novaconf='/etc/nova/nova.conf'
+(
+cat <<EE
+
+EE
+) > ${novaconf}
+fi
+
