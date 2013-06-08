b9d79afb14d1ebe9faa0f830d50ec96ef617b46b
root <root@rr-desktop.(none)>
2013-06-08 11:51:25 +0800

    144

--- autoinstall.sh
+++ autoinstall.sh
@@ -7,15 +7,17 @@ CONTROL_IP='172.18.200.103'
 . ${TOP}/functions
 set_ip
 if [ $? = 0 ];then
-set_hostname
+	set_hostname
 fi
 set_apt_sources
 if [ $? = 0 ];then
-#apt-get -y update && apt-get -y install ubuntu-cloud-keyring nova-compute nova-network
-echo "set success"	
+	apt-get -y update && apt-get -y install ubuntu-cloud-keyring && apt-get -y update && apt-get -y install nova-compute nova-network
+	echo "set success"	
+else
+        echo "error" && exit 44
 fi
 if [ $? = 0 ];then
-set_novaconfig && service nova-compute restart && service nova-network restart
-echo "nova config success"
+	set_novaconfig && service nova-compute restart && service nova-network restart
+	echo "nova config success"
 fi
 
